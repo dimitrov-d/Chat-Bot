41696c7cb99485182dfd7d4b393fe86568b62d9c
Damjan <damjandimitrov1@gmail.com>
2020-04-28 15:48:16 +0200

    Graphical elements styling done

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -3,12 +3,15 @@ import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class ChatBot extends Application
@@ -20,7 +23,7 @@ public class ChatBot extends Application
 		primaryStage.setTitle("Chat Bot");
 		GridPane gridPane = createGridPane();
 		addItems(gridPane);
-		Scene scene = new Scene(gridPane, 600, 700);
+		Scene scene = new Scene(gridPane, 535, 660);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		primaryStage.setOnCloseRequest(e -> Platform.exit());
@@ -28,14 +31,29 @@ public class ChatBot extends Application
 
 	public static void addItems(GridPane gridPane)
 	{
-		TextArea area = new TextArea();
-		area.setText("Bot: Hello!");
-		area.setEditable(false);
-		area.setPrefWidth(550);
-		area.setPrefHeight(550);
-		gridPane.add(area, 2, 0);
-		GridPane.setHalignment(area, HPos.CENTER);
-		GridPane.setMargin(area, new Insets(15, 0, 0, 5));
+		TextArea chat = new TextArea();
+		chat.setText("Bot: Hello!");
+		chat.setEditable(false);
+		chat.setPrefHeight(550);
+		chat.setPrefWidth(500);
+		chat.setFont(Font.font("Tw Cen MT Condensed", FontWeight.BOLD, 18));
+		gridPane.add(chat, 1, 0);
+		GridPane.setHalignment(chat, HPos.CENTER);
+		GridPane.setMargin(chat, new Insets(15, 0, 0, 15));
+		
+		TextArea input = new TextArea();
+		input.setPrefHeight(50);
+		input.setPrefWidth(30);
+		input.setPromptText("Enter your message here");
+		input.setFont(Font.font("Tw Cen MT Condensed", FontWeight.BOLD, 18));
+		gridPane.add(input, 1, 1);
+		GridPane.setMargin(input, new Insets(20, 50, 0, 15));
+		
+		Button sendButton = new Button("Send");
+		sendButton.setPrefHeight(63);
+		sendButton.setPrefWidth(100);
+		gridPane.add(sendButton, 2, 1);
+		GridPane.setMargin(sendButton, new Insets(20, 0, 0, -100));
 	}
 	
 
@@ -43,8 +61,6 @@ public class ChatBot extends Application
 	{
 		GridPane gridPane = new GridPane();
 		gridPane.setBackground(new Background(new BackgroundFill(Color.BURLYWOOD, CornerRadii.EMPTY, Insets.EMPTY)));
-		gridPane.setHgap(10);
-		gridPane.setVgap(80);
 		return gridPane;
 	}
 

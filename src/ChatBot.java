ee238cd1ed3de764651c02a22d4449eba0b0e3e2
Damjan <damjandimitrov1@gmail.com>
2020-04-28 14:26:10 +0200

    Added base graphical elements

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,10 +1,26 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class ChatBot
+public class ChatBot extends Application
 {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		primaryStage.setTitle("Chat Bot");
+		GridPane gridPane = new GridPane();
+		Scene scene = new Scene(gridPane, 600, 700);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setOnCloseRequest(e -> Platform.exit());
+	}
 
 	public static void main(String[] args)
 	{
-
+		launch(args);
 	}
 
 }
